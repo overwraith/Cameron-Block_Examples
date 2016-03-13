c81e03e699c8014623a067361b2e9f101cc6c381
overwraith <cnblock@cox.net>
2016-03-13 15:56:33 -0500

    RecursivePrint
    Adding a project for recursively printing a directory.

---
+++ RecursivePrint/RecursivePrint/Program.cs
@@ -0,0 +1,32 @@
+﻿/*Author: Cameron Block*/
+using System;
+using System.Threading.Tasks;
+
+
+namespace RecursivePrint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try {
+                //get starting directory
+                ArgParser parse = new ArgParser("d*", args);
+                string directory = parse.getString('d');
+
+                //really doesn't need to be parallel, is cool though. 
+                Parallel.ForEach<String>(FileUtil.RecurseFiles(directory), (String file) => {
+                    new Printer(file);
+                });
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(System.AppDomain.CurrentDomain.FriendlyName + " -d C:\\DirectoryToPrint");
+                Console.WriteLine("Prints recursively... ");
+            }
+
+        }//end main
+
+    }//end class
+
+}//end namespace
